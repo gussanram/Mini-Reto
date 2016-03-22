409cc5449bfc0e2e96540ba70e127b9ace4b3704
Gustavo <gustavo_santanaramirez@elcorteingles.com>
2016-03-22 22:38:16 +0000

    Initial Commit

---
+++ Mini Reto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mini Reto
+//
+//  Created by Gustavo Adolfo Santana Ramírez on 22/3/16.
+//  Copyright © 2016 Gustavo Adolfo Santana Ramírez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
